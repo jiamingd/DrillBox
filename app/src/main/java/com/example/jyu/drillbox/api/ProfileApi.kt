5eb6f0a975f92384de1cbfa8001a5c0b068ec65c
Jiaming Yu <jiamingd@yahoo.com>
2018-07-14 14:35:27 -0700

    Research on why api send request to LAN

--- app/src/main/java/com/example/jyu/drillbox/api/ProfileApi.kt
+++ app/src/main/java/com/example/jyu/drillbox/api/ProfileApi.kt
@@ -1,6 +1,7 @@
 package com.example.jyu.drillbox.api
 
 import android.content.Context
+import android.util.Base64
 import android.util.Log
 import com.example.jyu.drillbox.databases.AppDb
 import com.example.jyu.drillbox.objects.UserProfile
@@ -9,7 +10,6 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonNull
 import com.google.gson.JsonObject
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,17 +19,11 @@ import retrofit2.http.POST
 
 
 interface ProfileService {
-    @POST("auth/login/")
+    @POST("user/")
     fun login(
             @Header("Authorization") authorizationHeader: String?,
-            @Body body: JsonObject
+            @Body body: String
     ): Call<JsonObject>
-
-    @POST("auth/login/")
-    fun login2(
-            @Header("Authorization") authorizationHeader: String?,
-            @Body body: JsonObject
-    ): Call<ResponseBody>
 }
 
 class ProfileApi constructor(
@@ -41,45 +35,21 @@ class ProfileApi constructor(
      */
     fun login(username: String, password: String): String {
         try {
-            val r = profileService.login(null,
-                    jsonObject(
-                            "email_address" to username,
-                            "password" to  if (password?.length > 10) password else "Blabla"
-                    ))
+//            val basicAuthValue = username.trim() + ":" + password.trim()
+            val basicAuthValue = "jiamingd@yahoo.com:Address701"
+
+            val auth = "Basic " + Base64.encodeToString(basicAuthValue.toByteArray(), Base64.NO_WRAP)  //(value.getBytes("UTF-8")
+
+            val r = profileService.login(auth, "")
 
             r.enqueue(object : Callback<JsonObject> {
+
                 override fun onResponse(call: Call<JsonObject>?, response: Response<JsonObject>?) {
                     Log.i("", "")
-                    val token = response?.body()?.get("oauth_token")?.asString
-
-                    val fallBack = "{\"emails\":[{\"email\":\"jiaming.yu@evbqa.com\",\"verified\":true,\"primary\":true}],\"id\":\"226911043956\",\"name\":\"jiaming yu\",\"first_name\":\"jiaming\",\"last_name\":\"yu\",\"is_public\":false,\"image_id\":null,\"oauth_token\":\"5X5AWA2OFK4W3MEDOJ2X\"}"
-
-//                    val responseBody = response?.body()?:fallBack
-                    val responseBody = response?.body()?.toString()?:fallBack
-
-                    // Debug only
-// {"emails":[{"email":"jiaming.yu@evbqa.com","verified":true,"primary":true}],"id":"226911043956","name":"jiaming yu","first_name":"jiaming","last_name":"yu","is_public":false,"image_id":null,"oauth_token":"5X5AWA2OFK4W3MEDOJ2X"}
-//TODO: email collection resolve
-
-
-
-                    val userProfile = RetrofitTools.gson().fromJson(responseBody, UserProfile::class.java)
-
-                    //TODO: Should directly store to db
-                    val ups = AppDb.getInstance(context).userProfileDao().all()
-
-                    AppDb.getInstance(context).userProfileDao().update( userProfile )
-                    val ups2 = AppDb.getInstance(context).userProfileDao().all()
-                    ups2.observeForever {
-                        Log.i("", "size of UserProfile: " + it)
-
-                    }
-
-
                 }
 
                 override fun onFailure(call: Call<JsonObject>?, t: Throwable?) {
-                    Log.i("", "")
+                    Log.i("", "calling for authentication failed")
                 }
             })
 
@@ -90,52 +60,4 @@ class ProfileApi constructor(
         return ""
     }
 
-
-    companion object {
-        @JvmStatic
-        fun buildForInjection(context: Context): ProfileApi {
-            return ProfileApi(context)
-        }
-    }
 }
-
-//TODO: Below should later be used for login device object
-fun jsonObject(vararg params: Pair<String, Any?>, includeNulls: Boolean = true): JsonObject {
-    val json = JsonObject()
-    params.forEach {
-        val key = it.first
-        val value = it.second
-        when (value) {
-            null -> if (includeNulls) json.add(key, JsonNull.INSTANCE)
-            is String -> json.addProperty(key, value)
-            is Number -> json.addProperty(key, value)
-            is Boolean -> json.addProperty(key, value)
-            is Enum<*> -> json.addProperty(key, value.serializedName)
-            is List<*> -> json.add(key, jsonArray(*value.toTypedArray()))
-            is JsonElement -> json.add(key, value)
-        // feel free to add things in here
-            else -> throw RuntimeException("bad type ${value::class}")
-        }
-    }
-    return json
-}
-
-fun jsonArray(vararg entries: Any?): JsonArray {
-    val json = JsonArray()
-    entries.forEach {
-        when (it) {
-            null -> json.add(JsonNull.INSTANCE)
-            is String -> json.add(it)
-            is Boolean -> json.add(it)
-            is Number -> json.add(it)
-            is JsonElement -> json.add(it)
-            is Enum<*> -> json.add(it.serializedName)
-        // feel free to add things in here
-            else -> throw RuntimeException("bad type ${it::class}")
-        }
-    }
-    return json
-}
-
-val Enum<*>?.serializedName: String?
-    get() = EnumUtils.getSerializedName(this)
--- app/src/main/java/com/example/jyu/drillbox/api/RetrofitTools.kt
+++ app/src/main/java/com/example/jyu/drillbox/api/RetrofitTools.kt
@@ -1,55 +1,33 @@
 package com.example.jyu.drillbox.api
 
 import android.content.Context
-import android.util.Pair
-import com.example.jyu.drillbox.R
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 
 class RetrofitTools {
 
-
     companion object {
 
-        //TODO: Later on need to check how to integrate with
-        //                .addCallAdapterFactory(new PaginatedCallAdapter.Factory())
-        //                .addCallAdapterFactory(new SimpleCallAdapter.Factory())
-        //
         internal fun getRetrofit(context: Context): Retrofit {
             val httpClientBuilder = OkHttpClient.Builder()
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
                     .followRedirects(false)
                     .followSslRedirects(false)
 
-                    .addInterceptor {
-                        val requestBuilder = it.request().newBuilder()
-                        if (it.request().header("Authorization") == null) {
-                            requestBuilder.addHeader("Authorization", "Bearer PFFT4MYCYPF5EVJPHDJH")
-                        }
-                        requestBuilder.header("User-Agent", "User-Agent: EventbriteAttendeeAlpha/5.5.0-alpha(10000000) Android/24")
-                it.proceed(requestBuilder.build())
-                    }
-
-
             return Retrofit.Builder()
-                    .baseUrl("https://www.evbqaapi.com/v3/")
+                    .baseUrl("https://api.github.com/users/")
                     .client(httpClientBuilder.build())
                     .addConverterFactory(GsonConverterFactory.create())
-//                    .addCallAdapterFactory(new PaginatedCallAdapter.Factory())
-//                    .addCallAdapterFactory(SimpleCallAdapter.Factory())
                     .build()
         }
-
 
         // TODO: now use GsonTools.java to move on, figure out why can not ref.
         internal fun gson(): Gson {
@@ -61,11 +39,6 @@ class RetrofitTools {
             val builder = GsonBuilder()
                     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                     .serializeNulls()
-//            builder.registerTypeAdapterFactory(FlattenTypeAdapterFactory())
-//            builder.registerTypeAdapter(GregorianCalendar::class.java, CalendarTypeAdapter())
-//            builder.registerTypeAdapter(Calendar::class.java, CalendarTypeAdapter())
-//            builder.registerTypeAdapter(Date::class.java, JavaDateDeserializer())
-//            builder.registerTypeAdapter(Pair<*, *>::class.java, PairDeserializer())
             return builder
         }
     }
